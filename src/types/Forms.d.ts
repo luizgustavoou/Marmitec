25160f7b4c0947808c6ec9321e48cd28b20ebbe5
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-01-03 15:27:51 -0300

    fix(Forms.d.ts): corrigir tipagem

--- src/types/Forms.d.ts
+++ src/types/Forms.d.ts
@@ -1,10 +1,16 @@
-interface Proteinas {
-  fra_milanesa: number;
-  fra_assado: number;
-  figa_ace: number;
-  bis_sui_ace: number;
-  fra_molho: number;
-}
+import proteinas from "@/utils/proteinas";
+
+// type Proteinas<Type> = {
+//   [Property in keyof Type]: number;
+// };
+
+// type t2 = Proteinas<typeof proteinas>;
+
+// const t3: t2;
+
+type Proteinas = {
+  [Property in keyof typeof proteinas]: number;
+};
 
 export interface Form {
   proteinas: Proteinas;
