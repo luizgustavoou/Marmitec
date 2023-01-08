28b5de40b24b108fccceaaeec215e56531740e80
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-01-07 23:29:50 -0300

    feature(AppVue): adicionar template com footer

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,43 @@ const showMenu = ref(false);
 onMounted(() => {});
 </script>
 <template>
-  <TheMenu v-if="showMenu"></TheMenu>
-  <RouterView
-    @change-show-menu="(change: boolean) => (showMenu = change)"
-  ></RouterView>
+  <div class="d-flex flex-column h-100">
+    <header>
+      <TheMenu v-if="showMenu"></TheMenu>
+    </header>
+    <main class="flex-grow-1">
+      <RouterView
+        @change-show-menu="(change: boolean) => (showMenu = change)"
+      ></RouterView>
+    </main>
+    <footer
+      class="d-flex flex-column justify-content-center align-items-center text-white p-3"
+    >
+      <label> Feito por Luiz Gustavo </label>
+      <div>
+        <i class="bi bi-instagram me-2"></i>
+        <a href="https://www.instagram.com/luizgustavoou/" style="text-decoration: none; color: white;" target="_blank">luizgustavoou</a>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <style>
+html,
+body {
+  background-color: #e6e8eb;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  background-color: #e6e8eb; /*#e6e8eb; */
+  height: 100%;
+}
+
+footer {
+  background-color: #36454fa2;
+
 }
 
 label {
