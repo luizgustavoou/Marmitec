b11c9c1d336f536e667acac63a5af2111eff03f1
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-01-13 15:20:20 -0300

    fix(index.js): conectar ao websocket

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 //index.js: responsável por subir nosso servidor;
 require("dotenv").config();
-const app = require("./app");
 
-app.listen(process.env.PORT || 8124, () => {
+const app = require("./app");
+const appWs = require("./app-ws");
+
+const server = app.listen(process.env.PORT || 8124, () => {
   console.log(`Servidor conectado: http://${process.env.DB_HOST}:8124`);
 });
+
+appWs(server);
